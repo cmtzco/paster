36eb2075636ad57561161facc2c2d923d1c554ac
chris martinez <cmtz@mac.ubuntu>
2015-05-08 14:19:18 -0500

    Worked on config and the install script.

--- config.php
+++ config.php
@@ -1,7 +1,31 @@
 <?php
-
+/*
+* Add your database name here to ensure 
+* the tables are created and the correct
+* columns are made
+*/
 $database = "drl_paste";
+
+/*
+* Add the database user here for the 
+* the database specified above
+*/
 $user = "drl_paster";
+
+/*
+* Add the password here for the user
+* specified above
+*/
 $password = "pastepass";
+
+/*
+* Add the host for where the database
+* is hosted
+*/
 $host = "localhost";
+
+/*
+* Configure the URL that will be used
+* for the script
+*/
 $url = "http://drl.dev";
---
+++ install.php
@@ -0,0 +1,11 @@
+<body>
+<header>Install Paster</header>
+<form>
+<label for="url">URL: </label><input type="text" name="url" placeholder="http://url.com" /><br/>
+<label for="user">Username: </label><input type="text" name="user" /><br/>
+<label for="password">Password: </label><input type="text" name="password" /><br/>
+<label for="database">Database: </label><input type="text" name="database" /><br/>
+<label for="host">Host: </label><input type="text" name="host" /><br/>
+<input type="submit" name="submit" value="Submit" /><br/>
+</form>
+</body>
